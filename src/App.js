f937ede9664efe612357b2cab36aff3f078234da
TomasZ <tomas.z.henriquez@gmail.com>
2022-11-15 21:12:29 -0300

    test3

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import MiApi from './components/MiApi';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1> Digimon DB</h1>     
+        <h5>With this tool you can search Digimons by their Name or filter them by their evolution stage</h5>   
+      </header>
+
+      <div> <MiApi /> </div>
+
+      <footer>
+        <h3>@TomasZuñiga</h3>
+      </footer>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MiApi.jsx
@@ -0,0 +1,180 @@
+import { useState, useEffect } from "react";
+
+//falta aplicar sort , subirlo a github, y grabar el video.
+
+
+const MiApi = () => {
+  ////Estados & variables
+  const [dataApi, setDataApi] = useState([]); // obtiene y guarda datos de la api.
+  const [dataSelect, setDataSelect] = useState([]); // contiene el arreglo del select
+  const [search, setSearch] = useState(""); //contiene el arreglo del search
+  const [sort, setsort] = useState(false); //condicion de uso Sort
+
+  //estados necesarios para inputs
+  const [InputSearch, setInputSearch] = useState(""); // contiene el input del searchbar
+
+  const [select, setSelect] = useState(""); // contiene la opcion del select
+
+  //funciones enviar data a arreglos
+  const enviarSearch = (e) => {
+    e.preventDefault();
+    setSearch(dataApi.filter((nombre) => nombre.name == InputSearch));
+    console.log(search);
+    setSelect("");
+    setDataSelect("");
+  };
+  const enviarSelect = (e) => {
+    e.preventDefault();
+    setDataSelect(dataApi.filter((level) => level.level == select))
+    setsort(false)
+
+
+  };
+  const sortByName = () => {
+    
+    setDataSelect(dataSelect.sort((a, b) => a.name.localeCompare(b.name)))
+    setsort(true)
+
+  };
+
+
+  //.-funcion capturar data en inputs
+  const capturarInputSearch = (e) => {
+    setInputSearch(e.target.value);
+  };
+
+  const capturarSelect = (e) => {
+    setSelect(e.target.value);
+    console.log(select);
+    setSearch("");
+    setInputSearch("");
+  };
+  //use effect
+  useEffect(() => {
+    consultarApi();
+  }, []);
+
+  //consulta api
+  async function consultarApi() {
+    const res = await fetch("https://digimon-api.vercel.app/api/digimon");
+    const data = await res.json();
+    setDataApi(data);
+  }
+
+  return (
+    <div>
+      <div className="filters ms-5">
+        <div className="flex">
+          <div className="mb-3">
+            <input
+              className="form-control"
+              onChange={capturarInputSearch}
+              value={InputSearch}
+              id="exampleFormControlInput1"
+              placeholder="Search by Name"
+            />
+          </div>
+          <div>
+            <button className="btn btn-primary" onClick={enviarSearch}>
+              Search!
+            </button>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="dropdown">
+            <select
+              className="form-select"
+              onChange={capturarSelect}
+              aria-label="Default select example"
+            >
+              <option selected>Filter by Evolution Stage </option>
+              <option value="Fresh">Fresh</option>
+              <option value="In Training">In Training</option>
+              <option value="Rookie">Rookie</option>
+              <option value="Armor">Armor</option>
+              <option value="Champion">Champion</option>
+              <option value="Ultimate">Ultimate</option>
+              <option value="Mega">Mega</option>
+            </select>
+          </div>
+          <div>
+            <button className="btn btn-primary" onClick={enviarSelect}>
+              Apply Selection
+            </button>
+            <button className="btn btn-danger" onClick={sortByName}>
+              Sort by Name
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="wrap ">
+        {
+          //todos
+          dataSelect == "" && search == ""
+            ? dataApi.map((digimon) => (
+                <div className="card mb-3" key={digimon.name}>
+                  <a href={"https://digimon.fandom.com/wiki/" + digimon.name}>
+                    <img
+                      src={digimon.img}
+                      className="card-img-top"
+                      alt={digimon.name}
+                    />
+                  </a>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <h3 className="text-center">Name : {digimon.name}</h3>
+                    </li>
+                    <li className="list-group-item text-center">
+                      Stage: {digimon.level}
+                    </li>
+                  </ul>
+                </div>
+              ))
+            : //slo searchbar
+            search != "" //contiene el arreglo del searchbar
+            ? search.map((digimon) => (
+                <div className="card mb-3" key={digimon.name}>
+                  <a href={"https://digimon.fandom.com/wiki/" + digimon.name}>
+                    <img
+                      src={digimon.img}
+                      className="card-img-top"
+                      alt={digimon.name}
+                    />
+                  </a>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <h3 className="text-center">Name : {digimon.name}</h3>
+                    </li>
+                    <li className="list-group-item text-center">
+                      Stage: {digimon.level}
+                    </li>
+                  </ul>
+                </div>
+              ))
+            : // solo filtro
+              dataSelect.map((digimon) => (
+                <div className="card mb-3" key={digimon.name}>
+                  <a href={"https://digimon.fandom.com/wiki/" + digimon.name}>
+                    <img
+                      src={digimon.img}
+                      className="card-img-top"
+                      alt={digimon.name}
+                    />
+                  </a>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <h3 className="text-center">Name : {digimon.name}</h3>
+                    </li>
+                    <li className="list-group-item text-center">
+                      Stage: {digimon.level}
+                    </li>
+                  </ul>
+                </div>
+              ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default MiApi;
